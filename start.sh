925eb2d4b192c388ce287df6a810cbf15f62ecee
sujal shah <sujal@sujal.net>
2021-01-09 00:18:31 -0500

    initial checkin of the target command

--- start.sh
+++ start.sh
@@ -1,1 +1,7 @@
-echo Hello, world!
+#!/usr/bin/env bashio
+
+ecowitt2mqtt \
+  --mqtt-broker=192.168.1.101 \
+  --mqtt-username=user \
+  --mqtt-password=password \
+  --mqtt-topic=ecowitt2mqtt/device_1
